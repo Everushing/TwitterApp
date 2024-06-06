23b20b24b46b1cd113bbd196710140a47bf169b9
Evette Rushing <evette.ksr@gmail.com>
2024-06-06 15:23:04 -0400

    committ-update

---
+++ src/components/CreateTweetForm/index.jsx
@@ -0,0 +1,48 @@
+import { useState, useRef } from "react";
+import PropTypes from "prop-types";
+import styles from "./CreateTweetForm.module.css";
+import { Button, Form } from "react-bootstrap";
+
+function CreateTweetForm({ addTweet }) {
+  const [content, setContent] = useState("");
+const inputRef = useRef(null);
+  
+const handleSubmit = (e) => {
+    e.preventDefault();
+    
+if(inputRef.current.value==='') {
+  inputRef.current.focus();
+  //alert('Please fill this out');
+  return;
+}
+    
+    addTweet(content);
+    setContent("");
+  };
+
+  return (
+    <Form onSubmit={handleSubmit} className={styles.container}>
+      <Form.Label htmlFor="content">
+        <h4>Whats on your mind?</h4>
+      </Form.Label>
+
+      <Form.Control
+        className="mb-4"
+        type="text"
+        name="content"
+        id="content"
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        ref={inputRef}
+/>
+
+      <Button type ="submit">Tweet</Button>
+    </Form>
+  );
+}
+
+CreateTweetForm.propTypes = {
+  addTweet: PropTypes.func,
+};
+
+export default CreateTweetForm;
